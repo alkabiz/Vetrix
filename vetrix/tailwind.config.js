5657d61f5d01d1b617837bace5b6fddb0ad55c6d
Alkabiz Administrator <Alkabiz@hotmail.com>
2025-09-13 11:24:51 -0500

    chore: install required dependencies

---
+++ vetrix/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
+    "./contexts/**/*.{js,ts,jsx,tsx,mdx}",
+    "./hooks/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {},
+  plugins: [],
+}
