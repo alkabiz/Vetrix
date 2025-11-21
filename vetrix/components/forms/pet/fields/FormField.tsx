037849718d4baed52b38a2ed0caee8a040413eee
Alkabiz Administrator <Alkabiz@hotmail.com>
2025-11-21 16:15:09 -0500

    refactor: update logic in FormField.tsx

--- vetrix/components/forms/pet/fields/FormField.tsx
+++ vetrix/components/forms/pet/fields/FormField.tsx
@@ -3,7 +3,7 @@
 import React from "react"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils/index"
 import { shallowCompareProps } from "../utils/performance-utils"
 
 export interface FormFieldProps {
