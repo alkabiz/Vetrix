7a4d0bbd0719d619360c1f0c03f40ae39efc0748
Alkabiz Administrator <Alkabiz@hotmail.com>
2025-11-21 16:15:13 -0500

    refactor: update logic in FormSelect.tsx

--- vetrix/components/forms/pet/fields/FormSelect.tsx
+++ vetrix/components/forms/pet/fields/FormSelect.tsx
@@ -3,7 +3,7 @@
 import React from "react"
 import { Label } from "@/components/ui/label"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils/index"
 import { Loader2 } from "lucide-react"
 
 export interface SelectOption {
