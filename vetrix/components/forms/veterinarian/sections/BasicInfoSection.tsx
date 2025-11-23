a2aa41dfd2e540cb971fc0ac90122c7523c8372e
Alkabiz Administrator <Alkabiz@hotmail.com>
2025-11-23 12:26:55 -0500

    feat: create BasicInfoSection.tsx

---
+++ vetrix/components/forms/veterinarian/sections/BasicInfoSection.tsx
@@ -0,0 +1,118 @@
+import type React from "react"
+import { useFormContext } from "react-hook-form"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { FormField, FormItem, FormLabel, FormControl, FormMessage } from "@/components/ui/form"
+import type { VeterinarianFormValues } from "../VeterinarianForm.schema"
+
+export const BasicInfoSection: React.FC = () => {
+    const { control } = useFormContext<VeterinarianFormValues>()
+
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle className="text-lg">Basic Information</CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-4">
+                <div className="grid grid-cols-2 gap-4">
+                    <FormField
+                        control={control}
+                        name="employeeNumber"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Employee Number *</FormLabel>
+                                <FormControl>
+                                    <Input {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={control}
+                        name="licenseNumber"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>License Number *</FormLabel>
+                                <FormControl>
+                                    <Input {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <div className="grid grid-cols-2 gap-4">
+                    <FormField
+                        control={control}
+                        name="firstName"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>First Name *</FormLabel>
+                                <FormControl>
+                                    <Input {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={control}
+                        name="lastName"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Last Name *</FormLabel>
+                                <FormControl>
+                                    <Input {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <div className="grid grid-cols-2 gap-4">
+                    <FormField
+                        control={control}
+                        name="phone"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Phone</FormLabel>
+                                <FormControl>
+                                    <Input {...field} type="tel" />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input {...field} type="email" />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <FormField
+                    control={control}
+                    name="licenseExpiryDate"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>License Expiry Date</FormLabel>
+                            <FormControl>
+                                <Input {...field} type="date" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+            </CardContent>
+        </Card>
+    )
+}
