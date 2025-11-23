5861da4dcc6f0b64db8a67afc78bf79edaf635b7
Alkabiz Administrator <Alkabiz@hotmail.com>
2025-11-23 12:09:49 -0500

    refactor: update logic in PetFormView.tsx

--- vetrix/components/forms/pet/PetFormView.tsx
+++ vetrix/components/forms/pet/PetFormView.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import React from "react"
+import { Button } from "@/components/ui/button"
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 import { BasicInformationSection } from "./sections/BasicInformationSection"
 import { BirthAndAgeSection } from "./sections/BirthAndAgeSection"
