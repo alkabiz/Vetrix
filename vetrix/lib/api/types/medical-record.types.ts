6154511532b955f8c2148381f163199001523521
Alkabiz Administrator <Alkabiz@hotmail.com>
2025-12-01 14:47:50 -0500

    feat: Implement medical records management page with filters, statistics, table, and API integration.

--- vetrix/lib/api/types/medical-record.types.ts
+++ vetrix/lib/api/types/medical-record.types.ts
@@ -5,3 +5,6 @@ export interface MedicalRecordEntity extends MedicalRecord { }
 export interface MedicalRecordDTO extends MedicalRecordEntity {
     pet_name?: string
 }
+
+export type MedicalRecordInput = Omit<MedicalRecordDTO, "id" | "createdAt" | "updatedAt" | "pet_name">
+
